b65ce1f334b9b511c916868c6a76f6956270a82f
Felipe Sandoval <afsandovalhiguera@gmail.com>
2025-07-18 12:14:57 -0500

    CRUD Panel Admin

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -8,3 +8,6 @@ class Member(models.Model):
   gender = models.CharField(max_length=255, null=True)
   birthday= models.DateField(null=True)
   email= models.CharField(max_length=255, null=True)
+  
+  # def __str__(self):
+  #   return f"{self.firstname} {self.lastname}"
